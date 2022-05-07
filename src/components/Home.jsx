96a435fab572a182617fee2c17d43878d64f76d8
Neil <neilrowland56@yahoo.com>
2022-05-07 07:59:21 +0100

    Added data via data.js file. Completed initial Home page with mapping through products

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,12 +1,42 @@
 import React from 'react';
 import Header from './Header';
+import data from '../utils/data';
+import { Link } from 'react-router-dom';
 
 const Home = () => {
 	return (
 		<>
 			<Header />
-			<div className="w-full max-w-[1500px] mx-auto bg-blue-100 ">
-				<h1 className="text-3xl pt-[50px]">Product List</h1>
+			<div className="w-full max-w-[1500px] mx-auto ">
+				<h1 className="text-3xl pt-[80px] pb-5">Featured Products</h1>
+				<div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4  ">
+					{data.products.map((product) => (
+						<div
+							className="flex flex-col border border-[#404040] m-4 max-w-[410px
+							]"
+							key={product.slug}
+						>
+							<Link to={`/proroduct/${product.slug}`}>
+								<img
+									className="w-full max-w-[400px]"
+									src={product.image}
+									alt={product.name}
+								/>
+							</Link>
+							<div className="p-4">
+								<Link to={`/proroduct/${product.slug}`}>
+									<p>{product.name}</p>
+								</Link>
+								<p>
+									<strong> £ {product.price} </strong>
+								</p>
+								<button className="bg-[#f0c14b] rounded-md my-3 w-[150px] mx-auto p-2 border border-[#a88734]">
+									Add to cart
+								</button>
+							</div>
+						</div>
+					))}
+				</div>
 			</div>
 		</>
 	);
