b7004e9584fe367c4a0ef54a1f538113689d1098
Neil <neilrowland56@yahoo.com>
2022-05-07 12:11:48 +0100

    Fetch products from the backend

--- src/screens/HomeScreen.jsx
+++ src/screens/HomeScreen.jsx
@@ -1,16 +1,27 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Header from '../components/Header';
-import data from '../utils/data';
+// import data from '../utils/data';
 import { Link } from 'react-router-dom';
+import axios from 'axios';
 
 const Home = () => {
+	const [products, setProducts] = useState([]);
+
+	useEffect(() => {
+		const fetchData = async () => {
+			const result = await axios.get('/api/products');
+			setProducts(result.data);
+		};
+		fetchData();
+	}, []);
+
 	return (
 		<>
 			<Header />
 			<div className="w-full max-w-[1500px] mx-auto ">
 				<h1 className="text-3xl pt-[80px] pb-5">Featured Products</h1>
 				<div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4  ">
-					{data.products.map((product) => (
+					{products.map((product) => (
 						<div
 							className="flex flex-col border border-[#404040] m-4 max-w-[410px
 							]"
